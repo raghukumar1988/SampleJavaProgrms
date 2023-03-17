91110a26822c7b5496a92e7eafa4652a7a265274
ragrajam <careers.raghukumar@gmail.com>
2023-03-17 19:23:34 +0530

    adding few more changes

---
+++ src/main/java/designPatterns/builder/byusinglombok/Employee.java
@@ -0,0 +1,19 @@
+package designPatterns.builder.byusinglombok;
+
+import lombok.Builder;
+import lombok.ToString;
+
+@Builder
+//@Getter
+//@ToString
+public class Employee {
+
+    private final String firstName;    //required
+    //@NonNull
+    private final String lastName;
+    private final int age;    //required
+    private final int personalId; // required
+    private final String phone;    //optional
+    private final String address;    //optional
+    private final String mail;    //optional
+}
